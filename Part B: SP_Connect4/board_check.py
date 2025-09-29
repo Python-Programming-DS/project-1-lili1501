b1b548baa72a67d5373a40e41f104383ab8d151e
Shesadree <pshesadree151@gmail.com>
2025-09-28 23:42:01 -0400

    Minor changes

--- Part B: SP_Connect4/board_check.py
+++ Part B: SP_Connect4/board_check.py
@@ -14,27 +14,28 @@ def checkWin(board,turn):
     for i in range(6):
         for j in range(4):
              if board[i][j] == turn and board[i][j+1] == turn and board[i][j+2] == turn and board[i][j+3] == turn:
-                print(f"{turn} is the winner")
+                print(f"\n{turn} is the winner!")
                 return True
 
     # 6 rows and 7 cols, check each column for 4 continuous 'X' or 'O'
     for i in range(7):
         for j in range(3):
             if board[j][i] == turn and board[j+1][i] == turn and board[j+2][i] == turn and board[j+3][i] == turn:
-                    return True
+                print(f"\n{turn} is the winner!")
+                return True
                 
     #checking the diagnols from forward direction
     for i in range(3):
         for j in range(4):
             if board[i][j]==turn and board[i+1][j+1]==turn and board[i+2][j+2]==turn and board[i+3][j+3]==turn:
-                print(f"{turn} is the winner")
+                print(f"\n{turn} is the winner!")
                 return True
                             
     #checking the diagnols from backward direction
     for i in range(5,2,-1):
         for j in range(4):
             if board[i][j]==turn and board[i-1][j+1]==turn and board[i-2][j+2]==turn and board[i-3][j+3]==turn:
-                print(f"{turn} is the winner")
+                print(f"\n{turn} is the winner!")
                 return True
     return False
 
