bfe97655ec5236fc3be587e8841bda66e305e4fc
Shesadree <Shesadree@Shesh-MacBook-Air.local>
2025-09-28 22:38:15 -0400

    Recent changes and update

--- Part B: SP_Connect4/connect4_main.py
+++ Part B: SP_Connect4/connect4_main.py
@@ -1,55 +1,62 @@
-from board_status import display_board, reset_board
+from board_view import *
 from board_check import checkEnd
 
 #to validate the entry
-def validateEntry(row, col, board):
+def validateEntry(row, col, pos,board,available):
+
+    # Check if the user has selected from the available list
     # Check if row and col entered are in range
-    if row < 0 or row > 2 or col < 0 or col >2:
-        print(f"Invalid entry: try again.\nRow & column numbers must be either 0, 1, or 2")
+    if (pos not in available) or (row < 0 or row > 6 or col < 0 or col >7):
+        print(f"Invalid entry: try again.\nPlease input from the available position displayed")
+
     # Check if the cell is available
     elif board[row][col] != " ":
-        print(f"That cell is already taken.\nPlease make another selection.")
+        print(f"That cell is already taken.\nPlease make another selection from the available position displayed.")
+    
     else:
         print("Thankyou for your selection")
-        # display_board(board)
         return True
     return False
 
-
 def main():
-    
-    #Initializing the board (2D list)
     board = list()
-    for i in range(3):
-        board.append([" "] * 3)
-
+    for i in range(6):
+        board.append([" "] * 7)
+    
     another_game = 'y'
     while another_game.lower() == 'y':
+       
         print("New Game: X goes first\n")
         board = reset_board(board)
         display_board(board)
         End_game = False
         turn = 'X'
+        available = init_avail_pos()
         while End_game != True: 
             print(f"\n{turn}'s turn")
             print(f"Where do you want your {turn} placed?")
-            print("\033[0mPlease enter row number and column number separated by a comma:\033[91m", end="\n")
-            row, col = map(int, input().split(","))
-            print("\033[0m", end="")
-            print(f"You have entered row #{row}\n\t  and column #{col}")
-            if validateEntry(row, col, board) == True:
+
+            available = available_position(board,available)
+            print(f"Available Positions are : {available}\n")
+
+            pos = input("Please enter column-letter and row-number (e.g., a1): ")
+            row, col = col_row_num(pos)
+
+            if validateEntry(row, col,pos, board,available) == True:
                 board[row][col] = turn
+                
                 if checkEnd(board, turn)==True:
-                    # display_board(board)
                     End_game = True 
+                
                 display_board(board)   
+                
                 if turn == 'X':
                     turn = 'O'
                 else:
                     turn = 'X'
-        print("\n\033[0mAnother game? Enter either Y or y for yes:\033[91m", end="\n")
-        another_game = input()
-        print("\033[0m", end="")
+        
+        another_game = input("\nAnother game (y/n)? ")
+
     print("Thank you for playing!")
     
 if __name__ == "__main__":
